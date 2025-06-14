20358067d53c23ba29eac5349ceb17c45fef8428
Droid636 <2022043@utsh.edu.mx>
2025-06-13 20:11:11 -0600

    Consumir JSON

--- src/components/Jugadores.jsx
+++ src/components/Jugadores.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import './Jugadores.css';
+import React from 'react';
+import '../components/jugadores.css';
+import jugadores from '../data.json'; // Importa el array
 
 const Jugadores = () => {
   return (
-    <div className='Jugador'>
-      <img src='https://picsum.photos/180' alt=''></img>
+    <div className="jugadores-container">
+      {jugadores.map((jugador) => (
+        <div className="jugador-card" key={jugador.index}>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1200px-Bandera_de_Espa%C3%B1a.svg.png" alt={jugador.nombreJugador} />
+          <h2>{jugador.nombreJugador}</h2>
+          <p><strong>Nacionalidad:</strong> {jugador.nacionalidades.nacionalidad} / {jugador.nacionalidades.nacionalidad2}</p>
+          <p><strong>Edad:</strong> {jugador.edad}</p>
+          <p><strong>Club:</strong> {jugador.club}</p>
+          <p><strong>Valor de mercado:</strong> {jugador.valorDelMercado}</p>
+          <p><strong>Última revisión:</strong> {jugador.ultimaRevision}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Jugadores
+export default Jugadores;
