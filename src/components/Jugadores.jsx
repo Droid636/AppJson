7da2ed71b6b8ae1e8d61820968f6a0dd4ab09528
Droid636 <2022043@utsh.edu.mx>
2025-06-20 18:37:38 -0600

    Nacionalidades

--- src/components/Jugadores.jsx
+++ src/components/Jugadores.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import '../components/jugadores.css';
-import jugadores from '../data.json'; // Importa el array
+import jugadores from '../data.json';
 
 const Jugadores = () => {
   return (
     <div className="jugadores-container">
       {jugadores.map((jugador) => (
         <div className="jugador-card" key={jugador.index}>
-          <img  src={jugador.fotoJugador} />
+          <img src={jugador.fotoJugador} alt={jugador.nombreJugador} />
           <h2>{jugador.nombreJugador}</h2>
-          <p><strong>Nacionalidad:</strong> {jugador.nacionalidades.nacionalidad} / {jugador.nacionalidades.nacionalidad2}</p>
+          <p><strong>Nacionalidad:</strong> {jugador.nacionalidades}</p>
           <p><strong>Edad:</strong> {jugador.edad}</p>
           <p><strong>Club:</strong> {jugador.club}</p>
           <p><strong>Valor de mercado:</strong> {jugador.valorDelMercado}</p>
